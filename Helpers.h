24f051801621c707fd0ee9385050108fd35fadff
Rinat Omarov <ruslanasylbek@gmail.com>
2021-04-04 11:25:05 +0600

    Добавьте файлы проекта.

---
+++ Helpers.h
@@ -0,0 +1,10 @@
+
+int fun(int a, int b)
+{
+
+	int c;
+	c = a + b;
+	return c * c;
+
+
+}
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int result = fun (10,20);
+	std::cout << result << std::endl;
+}
